6c3bb0c3a2c3be3025b64bac99e32ee7d52f524a
Taofeek Adegoke <mydjangopythonprojects@gmail.com>
2022-07-14 18:52:49 +0100

    feature: register, login, update user profile

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("supersecret/", admin.site.urls),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.jwt")),
+    path("api/v1/profile/", include("apps.profiles.urls")),
 ] + static_urls
 
 
