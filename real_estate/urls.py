001b6eadd267c9d9eecf7676b2affed1058e49ea
Taofeek Adegoke <mydjangopythonprojects@gmail.com>
2022-07-17 16:36:27 +0100

    chore: setup docker with compose

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path("api/v1/properties/", include("apps.properties.urls")),
     path("api/v1/ratings/", include("apps.ratings.urls")),
     path("api/v1/enquiries/", include("apps.enquiries.urls")),
-] + static_urls
+]
 
 
 admin.site.site_header = "Real Estate Admin"
